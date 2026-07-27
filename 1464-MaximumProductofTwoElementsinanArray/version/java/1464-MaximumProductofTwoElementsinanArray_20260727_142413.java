16f758c98b9c8e70ba0c15c3e7e9af8c24a338f4
Ajith Kumar G <ajithkumar10022005@gmail.com>
2026-07-27 14:24:15 +0530

    Create 1464-MaximumProductofTwoElementsinanArray

---
+++ 1464-MaximumProductofTwoElementsinanArray/version/java/1464-MaximumProductofTwoElementsinanArray_20260727_142413.java
@@ -0,0 +1,13 @@
+// Last updated: 7/27/2026, 2:24:13 PM
+1class Solution {
+2    public int maxProduct(int[] nums) {
+3        int ans=0,max=0;
+4        for(int i=0;i<nums.length;i++){
+5            for(int j=i+1;j<nums.length;j++){
+6                ans=(nums[i]-1)*(nums[j]-1);
+7                max=Math.max(max,ans);
+8            }
+9        }
+10        return max;
+11    }
+12}
